922ce92137f709190e141a5b01b083088b6f7eba
ThaliaDiaz <thalidiaz24@gmail.com>
2017-11-09 11:53:16 -0500

    restringiendo casos para ganar el juego

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var square = document.querySelectorAll('td');
+
 window.onload = function() {
   var board = document.querySelector('.board-js');
   
@@ -17,7 +19,19 @@ function addJugada(event) {
   }
 }
 
-var matriz = document.getElementsByTagName('tr');
+function reset() {
+  for (var i = 0; i < square.length; i++) {
+    square[i].textContent = '';
+    isXTurn = true;
+    square[i].classList.remove('crossOut');
+    square[i].classList.remove('gameOver');
+  }
+  gameOver = false;
+  if (isXTurn) playerTurnDisplay.classList.add('xTurn');
+  boxesLeftToClick = 9;
+}
+
+var matriz = document.getElementsByTagName('td');
 for (i = 0; i <= matriz; i++) {
   for (j = 0; j < matriz[0]; j++) {
     
@@ -25,23 +39,22 @@ for (i = 0; i <= matriz; i++) {
 }
 
 
-/*
-if (document.getElementById('uno').firstChild === document.getElementById('dos').firstChild && document.getElementById('dos').firstChild === document.getElementById('tres').firstChild && document.getElementById('tres') !== '' && document.getElementById('uno') !== '' && document.getElementById('tres') !== '') {
-  document.write('El ganador es:' + getElementById('dos'));
-} else if (document.getElementById('cuatro').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('seis').firstChild) {
-  document.write('El ganador es:' + document.getElementById('cuatro'));
-} else if (document.getElementById('siete').firstChild === document.getElementById('ocho').firstChild && document.getElementById('ocho').firstChild === document.getElementById('nueve').firstChild) {
-  document.write('El ganador es:' + document.getElementById('siete'));
-} else if (document.getElementById('uno').firstChild === document.getElementById('cuatro').firstChild && document.getElementById('cuatro').firstChild === document.getElementById('siete').firstChild) {
-  document.write('El ganador es:' + document.getElementById('cuatro'));
-} else if (document.getElementById('dos').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('ocho').firstChild) {
+if (document.getElementById('uno').firstChild === document.getElementById('dos').firstChild && document.getElementById('dos').firstChild === document.getElementById('tres').firstChild && document.getElementById('tres') !== '' && document.getElementById('uno') !== '' && document.getElementById('dos') !== '') {
   document.write('El ganador es:' + document.getElementById('dos'));
-} else if (document.getElementById('tres').firstChild === document.getElementById('seis').firstChild && document.getElementById('seis').firstChild === document.getElementById('nueve').firstChild) {
+} else if (document.getElementById('cuatro').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('seis').firstChild && document.getElementById('cuatro') !== '' && document.getElementById('cinco') !== '' && document.getElementById('seis') !== '') {
+  document.write('El ganador es:' + document.getElementById('cuatro'));
+} else if (document.getElementById('siete').firstChild === document.getElementById('ocho').firstChild && document.getElementById('ocho').firstChild === document.getElementById('nueve').firstChild && document.getElementById('siete') !== '' && document.getElementById('ocho') !== '' && document.getElementById('nueve') !== '') {
+  document.write('El ganador es:' + document.getElementById('siete'));
+} else if (document.getElementById('uno').firstChild === document.getElementById('cuatro').firstChild && document.getElementById('cuatro').firstChild === document.getElementById('siete').firstChild && document.getElementById('uno') !== '' && document.getElementById('cuatro') !== '' && document.getElementById('siete') !== '') {
+  document.write('El ganador es:' + document.getElementById('cuatro'));
+} else if (document.getElementById('dos').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('ocho').firstChild && document.getElementById('dos') !== '' && document.getElementById('cinco') !== '' && document.getElementById('ocho') !== '') {
+  document.write('El ganador es:' + document.getElementById('dos'));
+} else if (document.getElementById('tres').firstChild === document.getElementById('seis').firstChild && document.getElementById('seis').firstChild === document.getElementById('nueve').firstChild && document.getElementById('tres') !== '' && document.getElementById('seis') !== '' && document.getElementById('nueve') !== '') {
   document.write('El ganador es:' + document.getElementById('seis'));
-} else if (document.getElementById('uno').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('nueve').firstChild) {
+} else if (document.getElementById('uno').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('nueve').firstChild && document.getElementById('uno') !== '' && document.getElementById('cinco') !== '' && document.getElementById('nueve') !== '') {
   document.write('El ganador es:' + document.getElementById('cinco'));
-} else if (document.getElementById('tres').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('siete').firstChild) {
+} else if (document.getElementById('tres').firstChild === document.getElementById('cinco').firstChild && document.getElementById('cinco').firstChild === document.getElementById('siete').firstChild && document.getElementById('tres') !== '' && document.getElementById('cinco') !== '' && document.getElementById('siete') !== '') {
   document.write('El ganador es:' + document.getElementById('cinco'));
 } else {
   document.write('no hay ganador');
-}*/
+}
